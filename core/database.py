4420f60ec87b0ea9c2e27bebbcd7a43233849760
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2026-09-10 18:26:24 +0300

    core: add get_db_conn method

--- core/database.py
+++ core/database.py
@@ -1,13 +1,15 @@
 import os
 import certifi
 from dotenv import load_dotenv
+import psycopg
+from psycopg.rows import dict_row
 
 load_dotenv()
 
 os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
-def get_database_url():
+def _get_database_url():
     database_url = os.getenv("DATABASE_URL")
 
     if not database_url:
@@ -20,3 +22,15 @@ def get_database_url():
         database_url = f"{database_url}{separator}sslmode=require"
 
     return database_url
+
+
+def get_db_conn():
+    DATABASE_URL = _get_database_url()
+
+    conn = psycopg.connect(
+        conninfo=DATABASE_URL,
+        autocommit=True,
+        row_factory=dict_row
+    )
+
+    return conn 
