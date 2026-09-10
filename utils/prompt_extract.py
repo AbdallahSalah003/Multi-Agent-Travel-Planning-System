55b39556f170a19074dc48c1cfd3fc13d891731f
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2026-09-11 00:24:41 +0300

    util: extract_destination method

---
+++ utils/prompt_extract.py
@@ -0,0 +1,15 @@
+from core.llm import llm
+
+def extract_destination(query: str):
+    prompt = f"""
+    Extract only the destination city or country.
+
+    Query:
+    {query}
+
+    Return only destination name.
+    """
+
+    response = llm.invoke(prompt)
+
+    return response.content.strip()
