460a1c38c20b46f8604edf860773a832a55e0faf
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2026-09-10 17:52:57 +0300

    feat: add flight agent

---
+++ agents/flight.py
@@ -0,0 +1,16 @@
+from memory.state import TravelState
+from tools.flight import search_flights
+from langchain_core.messages import AIMessage
+
+
+def flight_agent(state: TravelState):
+    query = state["user_query"]
+    flight_data = search_flights(query)
+
+    return {
+        "flight_results": flight_data,
+        "messages": [
+            AIMessage(content="Flight results fetched.")
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
